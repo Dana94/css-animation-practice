73850456dc3ce28218269381ab6a57c290ecd19b
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-09-02 18:15:19 -0400

    Implement door opening effect

---
+++ src/components/DoorEffect.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <div class="square red">
+      <div class="square blue"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DoorEffect",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.square {
+  height: 200px;
+  width: 200px;
+}
+
+.blue {
+  background-color: blue;
+  animation: open 2s linear forwards;
+}
+
+.red {
+  background-color: red;
+  margin: 0 auto;
+}
+
+@keyframes open {
+  0% {
+    transform: rotateY(180deg);
+  }
+
+  100% {
+    transform: translateX(200px);
+    background-color: pink;
+  }
+}
+</style>
